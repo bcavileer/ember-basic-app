cd91a6b1a468f62e07cd4ef067b69af40bfa20e5
Rob Sturcke <rs@rsvision.com>
2017-04-04 13:52:30 -0400

    Base commit

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+// Generic Comment
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
