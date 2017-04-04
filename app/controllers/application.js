359ac96dc6840b22038d8b2ef5288540480b8396
Rob Sturcke <rs@rsvision.com>
2017-04-04 15:13:57 -0400

    input field example with double binding exercise

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     name: 'Rob',
-    greeting: 'Hey soon to be Ember Master:'
+    greeting: 'Hey soon to be Ember Master: ',
+    sport: ''
 });
