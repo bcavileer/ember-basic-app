653c756fe0c4d8adfa9062229b92b43c7b848d20
Rob Sturcke <rs@rsvision.com>
2017-04-12 11:42:33 -0400

    chef challenge for interactivity actions

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,20 +1,45 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    foods: [
-      {name: 'Tacos', isAvailable: true},
-      {name: 'Salad', isAvailable: true},
-      {name: 'Pizza', isAvailable: false},
-      {name: 'Vegetables', isAvailable: true},
-      {name: 'Fruit Smoothies', isAvailable: false}
+  chefs: [
+    {name: 'Anthony', isAvailable: true},
+    {name: 'Gordon', isAvailable: false},
+    {name: 'Smacky', isAvailable: true},
+    {name: 'McFinney', isAvailable: false}
   ],
-    restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3},
-    actions: {
-      makeUnavailable(food){
-        Ember.set(food, 'isAvailable', false)
-      },
-      makeAvailable(food){
-        Ember.set(food, 'isAvailable', true)
-      }
+
+  actions: {
+    makeUnavailable(chef){
+      Ember.set(chef, 'isAvailable', false)
+    },
+
+    makeAvailable(chef){
+      Ember.set(chef, 'isAvailable', true)
     }
+  }
+
+
+
+
+
+
+
+
+
+  //   foods: [
+  //     {name: 'Tacos', isAvailable: true},
+  //     {name: 'Salad', isAvailable: true},
+  //     {name: 'Pizza', isAvailable: false},
+  //     {name: 'Vegetables', isAvailable: true},
+  //     {name: 'Fruit Smoothies', isAvailable: false}
+  // ],
+  //   restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3},
+  //   actions: {
+  //     makeUnavailable(food){
+  //       Ember.set(food, 'isAvailable', false)
+  //     },
+  //     makeAvailable(food){
+  //       Ember.set(food, 'isAvailable', true)
+  //     }
+  //   }
 });
