b261042e55b48c2e081c0300d32847a90b9b4061
Rob Sturcke <rs@rsvision.com>
2017-04-12 10:02:07 -0400

    chef tracker if block statements challenge

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    foods: [
-      {name: 'Tacos', isAvailable: true},
-      {name: 'Salad', isAvailable: true},
-      {name: 'Pizza', isAvailable: false},
-      {name: 'Vegetables', isAvailable: true},
-      {name: 'Fruit Smoothies', isAvailable: false}
-  ],
-    restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
+        chefs: [
+            {name: 'Anthony', isAvailable: true},
+            {name: 'Gordon', isAvailable: true},
+            {name: 'Jim', isAvailable: false},
+            {name: 'Sam', isAvailable: false}
+        ]
+
+  //   foods: [
+  //     {name: 'Tacos', isAvailable: true},
+  //     {name: 'Salad', isAvailable: true},
+  //     {name: 'Pizza', isAvailable: false},
+  //     {name: 'Vegetables', isAvailable: true},
+  //     {name: 'Fruit Smoothies', isAvailable: false}
+  // ],
+  //   restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
 });
