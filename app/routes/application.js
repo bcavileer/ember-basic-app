66d5ea284bfe277f1f4dd5835c072fbe7016508c
Rob Sturcke <rs@rsvision.com>
2017-04-12 15:22:19 -0400

    not connecting to firebase

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return [
-      {name: 'Anthony', isAvailable: true},
-      {name: 'Gordon', isAvailable: true},
-      {name: 'Crots', isAvailable: false},
-      {name: 'Oryx', isAvailable: true},
-      {name: 'Aetheon', isAvailable: false}
-    ]
+    return this.store.findAll('chef')
   }
 });
