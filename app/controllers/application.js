4d0ca2d43567d2071fbf1d874428b0f0d9b055f7
Rob Sturcke <rs@rsvision.com>
2017-04-12 09:47:32 -0400

    if for Availability of stock

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,20 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-      chefs: [
-          {name: 'Anothony Bourdain', isAvailable: true},
-          {name: 'Gordon Ramsey', isAvailable: true},
-          {name: 'Some Dude', isAvailable: false},
-          {name: 'Some Chick', isAvailable: false}
-      ]
-
-
-  //   foods: [
-  //     {name: 'Tacos', isAvailable: true},
-  //     {name: 'Salad', isAvailable: true},
-  //     {name: 'Pizza', isAvailable: false},
-  //     {name: 'Vegetables', isAvailable: true},
-  //     {name: 'Fruit Smoothies', isAvailable: false}
-  // ],
-  //   restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
+    foods: [
+      {name: 'Tacos', isAvailable: true},
+      {name: 'Salad', isAvailable: true},
+      {name: 'Pizza', isAvailable: false},
+      {name: 'Vegetables', isAvailable: true},
+      {name: 'Fruit Smoothies', isAvailable: false}
+  ],
+    restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
 });
