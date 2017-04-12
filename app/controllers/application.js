8ea2a47dac383e2525e9cc33428871fee5003f7d
Rob Sturcke <rs@rsvision.com>
2017-04-12 10:52:31 -0400

    back to menu tracker ex

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,21 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-      chefs: [
-        {name: 'Anothony', isAvailable: true},
-        {name: 'Gordon', isAvailable: true},
-        {name: 'Kim', isAvailable: false},
-        {name: 'Jeff', isAvailable: false},
-        {name: 'Gnarzack', isAvailable: true}
-      ]
-
-
-//     foods: [
-//       {name: 'Tacos', isAvailable: true},
-//       {name: 'Salad', isAvailable: true},
-//       {name: 'Pizza', isAvailable: false},
-//       {name: 'Vegetables', isAvailable: true},
-//       {name: 'Fruit Smoothies', isAvailable: false}
-//   ],
-//     restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
+    foods: [
+      {name: 'Tacos', isAvailable: true},
+      {name: 'Salad', isAvailable: true},
+      {name: 'Pizza', isAvailable: false},
+      {name: 'Vegetables', isAvailable: true},
+      {name: 'Fruit Smoothies', isAvailable: false}
+  ],
+    restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
 });
