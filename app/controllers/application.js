92827aaa4f824cff1ea6e9ca88b2ad9947683f04
Rob Sturcke <rs@rsvision.com>
2017-04-12 09:35:02 -0400

    chef tracker challenge for multiple properties

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    foods: [
-      {name: 'Tacos', isAvailable: true},
-      {name: 'Salad', isAvailable: true},
-      {name: 'Pizza', isAvailable: false},
-      {name: 'Vegetables', isAvailable: true},
-      {name: 'Fruit Smoothies', isAvailable: false}
-  ],
-    restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
+      chefs: [
+          {name: 'Anothony Bourdain', isAvailable: true},
+          {name: 'Gordon Ramsey', isAvailable: true},
+          {name: 'Some Dude', isAvailable: false},
+          {name: 'Some Chick', isAvailable: false}
+      ]
+
+
+  //   foods: [
+  //     {name: 'Tacos', isAvailable: true},
+  //     {name: 'Salad', isAvailable: true},
+  //     {name: 'Pizza', isAvailable: false},
+  //     {name: 'Vegetables', isAvailable: true},
+  //     {name: 'Fruit Smoothies', isAvailable: false}
+  // ],
+  //   restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
 });
