de5c64ff05c9a358551727a6291e577756d8f575
Rob Sturcke <rs@rsvision.com>
2017-04-12 10:50:33 -0400

    chef challenge with css importing

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,19 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-        chefs: [
-            {name: 'Anthony', isAvailable: true},
-            {name: 'Gordon', isAvailable: true},
-            {name: 'Jim', isAvailable: false},
-            {name: 'Sam', isAvailable: false}
-        ]
+      chefs: [
+        {name: 'Anothony', isAvailable: true},
+        {name: 'Gordon', isAvailable: true},
+        {name: 'Kim', isAvailable: false},
+        {name: 'Jeff', isAvailable: false},
+        {name: 'Gnarzack', isAvailable: true}
+      ]
 
-  //   foods: [
-  //     {name: 'Tacos', isAvailable: true},
-  //     {name: 'Salad', isAvailable: true},
-  //     {name: 'Pizza', isAvailable: false},
-  //     {name: 'Vegetables', isAvailable: true},
-  //     {name: 'Fruit Smoothies', isAvailable: false}
-  // ],
-  //   restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
+
+//     foods: [
+//       {name: 'Tacos', isAvailable: true},
+//       {name: 'Salad', isAvailable: true},
+//       {name: 'Pizza', isAvailable: false},
+//       {name: 'Vegetables', isAvailable: true},
+//       {name: 'Fruit Smoothies', isAvailable: false}
+//   ],
+//     restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3}
 });
