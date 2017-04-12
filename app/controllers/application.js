f3c8327ad9a411f6a32885474206eee6d155f380
Rob Sturcke <rs@rsvision.com>
2017-04-12 13:17:54 -0400

    food menu tracker ex original with model hooks

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,28 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3},
     actions: {
-        makeUnavailable(chef){
-          Ember.set(chef, 'isAvailable', false)
-        },
-        makeAvailable(chef){
-          Ember.set(chef, 'isAvailable', true)
-        }
+      makeUnavailable(food){
+        Ember.set(food, 'isAvailable', false)
+      },
+      makeAvailable(food){
+        Ember.set(food, 'isAvailable', true)
+      }
     }
-
-
-
-
-
-
-
-    // restaurant: {name: 'I don\'t know, the place your girlfriend is always talking about', yearsOpen: 3},
-    // actions: {
-    //   makeUnavailable(food){
-    //     Ember.set(food, 'isAvailable', false)
-    //   },
-    //   makeAvailable(food){
-    //     Ember.set(food, 'isAvailable', true)
-    //   }
-    // }
 });
